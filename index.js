b5eb988a20a5759b526c96f3514ef8e3539c704b
Brocks1445 <87440797+Brocks1445@users.noreply.github.com>
2021-08-22 19:25:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+Sample = "\x1b[33m"
+Sample2 = "\x1b[4m"
+console.log(Sample + Sample2 + "Made By Brocks")
---
+++ main.py
@@ -0,0 +1,4 @@
+# Colorama Required, pip install colourama
+from colorama import Fore, Back, Style
+
+print(Fore.YELLOW + "Made By Brocks")
